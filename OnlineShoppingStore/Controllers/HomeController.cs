f4294dba6d2bc3f27f1963ed69c7aeaf6cf919b4
monirulislam14 <damfast14@gmail.com>
2019-11-25 20:36:47 +0600

    Ecomerce-website

---
+++ OnlineShoppingStore/Controllers/HomeController.cs
@@ -0,0 +1,87 @@
+﻿using OnlineShoppingStore.DAL;
+using OnlineShoppingStore.Models;
+using OnlineShoppingStore.Models.Home;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineShoppingStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        dbMyOnlineShoppingEntities context = new dbMyOnlineShoppingEntities();
+        public ActionResult Index(string search,int? page)
+        {
+            HomeIndexViewModel model = new HomeIndexViewModel();
+            return View(model.CreateModel(search, 4, page));
+        }
+        public ActionResult Checkout()
+        {
+            return View();
+        }
+        public ActionResult CheckoutDetails()
+        {
+            return View();
+        }
+        public ActionResult AddToCart(int productId)
+        {
+            if (Session["cart"] == null)
+            {
+                List<Item> cart = new List<Item>();
+                var product = context.Tbl_Product.Find(productId);
+                cart.Add(new Item()
+                {
+                    Product = product,
+                    Quantity = 1
+                });
+                Session["cart"] = cart;
+            }
+            else
+            {
+                List<Item> cart = (List<Item>)Session["cart"];
+                var product = context.Tbl_Product.Find(productId);
+                foreach (Item item in cart)
+                {
+                    if (item.Product.ProductId== productId)
+                    {
+                        int prevQty = item.Quantity;
+                        cart.Remove(item);
+                        cart.Add(new Item()
+                        {
+                            Product = product,
+                            Quantity = prevQty + 1
+                        });
+                        break;
+                    }
+                    else
+                    {
+                        cart.Add(new Item()
+                        {
+                            Product = product,
+                            Quantity = 1
+                        });
+                        break;
+                    }
+                }
+                Session["cart"] = cart;
+            }
+            return Redirect("Index");
+        }
+        public ActionResult RemoveFromCart(int productId)
+        {
+            List<Item> cart = (List<Item>)Session["cart"];
+            foreach (var item in cart)
+            {
+                if (item.Product.ProductId == productId)
+                {
+                    cart.Remove(item);
+                    break;
+                }
+            }
+            Session["cart"] = cart;
+            return Redirect("Index");
+        }
+    }
+}
---
+++ OnlineShoppingStore/Models/Home/HomeIndexViewModel.cs
@@ -0,0 +1,34 @@
+﻿using OnlineShoppingStore.DAL;
+using OnlineShoppingStore.Repository;
+using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace OnlineShoppingStore.Models.Home
+{
+    public class HomeIndexViewModel
+    {
+        public GenericUnitOfWork _unitOfWork = new GenericUnitOfWork();
+        public IPagedList<Tbl_Product> ListOfProducts { get; set; }
+        dbMyOnlineShoppingEntities context = new dbMyOnlineShoppingEntities();
+        public HomeIndexViewModel CreateModel(string search, int pageSize, int? page)
+        {
+            
+                SqlParameter[] param = new SqlParameter[] {
+                   new SqlParameter("@search",search??(object)DBNull.Value)
+                   };
+               IPagedList<Tbl_Product> data = context.Database.SqlQuery<Tbl_Product>("GetBySearch @search", param).ToList().ToPagedList(page ?? 1, pageSize);
+                return new HomeIndexViewModel
+                {
+                    ListOfProducts = data
+                };
+
+            
+        }
+       
+    }
+}
+    
